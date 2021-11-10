ecdeb0341ff49a19bd055ed769c4676349f7906c
Gavin Crowley <gavincrowley@gmail.com>
2021-11-10 22:23:13 +0000

    Nov 10

--- sandbox.py
+++ sandbox.py
@@ -12,18 +12,25 @@
 
 # fn(n)
 
-def substring_copy(str, n):
-    flen = 2
-    if flen > len(str):
-        flen = len(str)
-    substr = str[:flen]
+# def substring_copy(str, n):
+#     flen = 2
+#     if flen > len(str):
+#         flen = len(str)
+#     substr = str[:flen]
 
-    result = ""
-    for i in range(n):
-        result = result + substr
-    return result
+#     result = ""
+#     for i in range(n):
+#         result = result + substr
+#     return result
 
 
-print(substring_copy('abcdef', 2))
-print(substring_copy('p', 3))
-print(substring_copy('pethujkol', 7))
+# print(substring_copy('abcdef', 2))
+# print(substring_copy('p', 3))
+# print(substring_copy('pethujkol', 7))
+a = "happy"
+b = "sad"
+
+pay = input("How much did you earn this week: ")
+c = a if (int(pay) > 500) else b
+
+print(c)
