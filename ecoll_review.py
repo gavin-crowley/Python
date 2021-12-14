6117db51f8b56189ae720ca4bb7a78a59c46c093
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2021-12-14 21:15:26 +0000

    Dec 14

---
+++ ecoll_review.py
@@ -0,0 +1,73 @@
+# functions can access variables declared outside the function
+
+# keyword arguments need to be specified at function definition time
+
+def test():
+    pass
+
+test()
+
+# ordered arguments are not valid types of function arguments
+
+# variable length keyword arguments are passed into the function as a dictionary
+
+#Primitive Data Structures
+#Integers
+#Float
+#Strings
+#Boolean
+
+#Non-Primitive Data Structures
+#Arrays
+#Lists
+#Tuples
+#Dictionary
+#Sets
+#Files
+
+x = 3
+y = 4
+
+def add(a, b):
+    result = x + y
+    print(result)
+
+add(10, 20)
+
+
+# function code is not executed when defined
+# a function can have multiple return values
+# a function can have multiple return statments
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ py_cc/9.py
@@ -0,0 +1,131 @@
+# class Dog:
+#     """A simple attempts to model a dog."""
+
+#     def __init__(self, name, age):
+#         """Initialize name and age attributes."""
+#         self.name = name
+#         self.age =  age
+        
+#     def sit(self):
+#         """Simulate a dog sitting in response to a command."""
+#         print(f"{self.name} is now sitting.")
+
+#     def roll_over(self):
+#         """Simulate rolling over in response to a command."""
+#         print(f"{self.name} rolled over!")
+
+# my_dog = Dog('Willie', 6)
+# your_dog = Dog('Lucy', 3)
+
+# print(f"My dog's name is {my_dog.name}.")
+# print(f"My dog is {my_dog.age} years old.")
+# my_dog.sit()
+
+# print(f"\nYour dog's name is {your_dog.name}.")
+# print(f"Your dog is {your_dog.age} years old.")
+# your_dog.sit()
+
+#1
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name} does {self.cuisine_type} food.")
+    
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is open!")
+
+restaurant = Restaurant('Golden Balls', 'Chinese')
+
+print(f"The restaurant's name is {restaurant.restaurant_name}, \
+    which is a {restaurant.cuisine_type} restaurant.")
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
+
+
+#2
+
+indian = Restaurant('The Taj', 'Indian')
+indian.describe_restaurant()
+
+afghan = Restaurant('Graveyard of Pies', 'Afghan')
+afghan.describe_restaurant()
+
+phillipino = Restaurant('Thousand Islands', 'Phillipino')
+phillipino.describe_restaurant()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
