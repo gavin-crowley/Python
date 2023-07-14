de1a9c56dfc82ba5b1fd401da381a14dd2d61049
Gavin Crowley <gavincrowley@gmail.com>
2023-07-14 19:24:20 +0100

    py practice

---
+++ Py_Practice/hackerrank.py
@@ -0,0 +1,43 @@
+# n = 22
+# if n % 2 != 0:
+#     print("Weird")
+# elif n % 2 == 0 and n >= 2 and n <= 5:
+#     print("Not Weird")
+# elif n % 2 == 0 and n >= 6 and n <= 20:
+#     print("Weird")
+# elif n % 2 == 0 and n > 20:
+#     print("Not Weird")
+
+# n = 3
+# for i in range(0, n):
+#     print(i**2)
+
+# if (n % 2 != 0):
+#         print("Weird")
+#     else:
+#         if n>=2 and n<=5:
+#             print("Not Weird")
+#         elif n<=20:
+#             print("Weird")
+#         else:
+#             print("Not Weird")
+
+
+
+# year = 1990
+
+# if year % 100 == 0 and year % 400 == 0:
+#     print(True)
+# elif year % 100 == 0:
+#     print(False)
+# elif year % 4 == 0:
+#     print(True)
+
+
+# n = 5
+# i = 0
+
+# for i in range(1, n + 1):
+#     print(i, end="")
+# # 12345
+
