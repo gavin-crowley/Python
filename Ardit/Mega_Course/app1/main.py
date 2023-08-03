23060eeaa1ad63a052e945c189290a3ca335ecf3
Gavin Crowley <gavincrowley@gmail.com>
2023-08-03 16:39:15 +0100

    app1 start

---
+++ Ardit/Mega_Course/app1/main.py
@@ -0,0 +1,34 @@
+
+
+# user_input = ''
+# while user_input != 'done':
+#     user_input = input("Enter a todo: ")
+#     print(user_input)
+#
+# input_status = ''
+# user_input = ''
+# todo_list = []
+# while input_status != 'done':
+#     user_input = input("Enter a todo: ")
+#     if user_input == 'done':
+#         input_status = user_input
+#     else:
+#         todo_list.append(user_input)
+#
+# print(todo_list)
+
+
+# user_prompt = "Enter a todo:"
+# todo1 = input(user_prompt)
+# todo2 = input(user_prompt)
+# todo3 = input(user_prompt)
+#
+# todos = [todo1, todo2, todo3]
+# print(todos)
+#
+# print(type(todo1))
+
+book_title = input("Enter title: ")
+print('Title length is', len(book_title))
+
+
