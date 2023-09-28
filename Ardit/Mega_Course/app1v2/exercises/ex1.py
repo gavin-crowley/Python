07edf06ac2a95467d30cf15a524e030e60f7197d
Gavin Crowley <gavincrowley@gmail.com>
2023-09-28 12:56:09 +0100

    day 1

---
+++ Ardit/Mega_Course/app1v2/exercises/ex1.py
@@ -0,0 +1,14 @@
+# Day 1
+# # 1
+# any_name = "a string"
+# print(type(any_name))
+#
+# # 2
+# user_input = input("Enter input: ")
+# print(user_input)
+#
+# # 3
+# my_list = ['a', 'b', 'c', 'd']
+
+my_answer = input("What is your answer?")
+answers = ['Yes', 'No', 'Yes', 'No', my_answer]
